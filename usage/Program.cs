71ce2815437d19cba57f3127e4b841dff62a9cf3
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-02-19 10:43:46 -0300

    usage

---
+++ usage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Idiomatic;
+namespace usage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            Fooer.Foo("Hello World!");
+        }
+    }
+}
